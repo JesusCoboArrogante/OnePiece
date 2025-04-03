1c9658f1baf55d764d7a3ebbf670dd0c1f002028
Jesus Cobo <146002181+JesusCoboArrogante@users.noreply.github.com>
2025-04-03 17:35:33 +0200

    implementacion de la librerias

---
+++ OnePiece/build.gradle.kts
@@ -0,0 +1,42 @@
+plugins {
+    kotlin("jvm") version "2.0.20"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    //base de datos en h2
+    implementation("com.h2database:h2:2.3.232")
+
+    //JDBI
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jbi:jdbi3:jdbi-core:3.48.0")
+    implementation("org.jdbi:jdbi3:jdbi-sqlobject:3.48.0")
+    implementation("org.jdbi:jdbi3-kotlin:3.48.0")
+    implementation("org.jdbi:jdbi3-kotlin-sqlobject:3.48.0")
+
+    //logger
+    implementation("org.lighthousegames:loggin:1.5.0")
+    implementation("ch.qos.logback:logback-classic:1.5.12")
+
+    //cache caffeine
+    implementation("com.github.ben-manes.caffeine:caffeine:3.2.0")
+
+    //test
+    testImplementation("io.mockk:mockk:1.13.16")
+    testImplementation(kotlin("test"))
+    testImplementation("org.jdbi:jdbi3-testing:3.48.0")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
