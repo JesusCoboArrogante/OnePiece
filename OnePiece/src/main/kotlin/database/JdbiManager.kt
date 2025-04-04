96a754b6a88c07f1ef7cce063d541a88f93a2d7e
Jesus Cobo <146002181+JesusCoboArrogante@users.noreply.github.com>
2025-04-04 19:25:26 +0200

    creacion de la base de datos

--- OnePiece/src/main/kotlin/database/JdbiManager.kt
+++ OnePiece/src/main/kotlin/database/JdbiManager.kt
@@ -1,17 +1,55 @@
 package org.example.database
 
+import org.example.config.Config
 import org.jdbi.v3.core.Jdbi
+import org.jdbi.v3.core.kotlin.KotlinPlugin
+
+import org.jdbi.v3.sqlobject.SqlObjectPlugin
 import org.lighthousegames.logging.logging
+import java.io.File
 
 class JdbiManager private constructor(){
     private val log = logging()
 
+    private val jdbi = Jdbi.create(Config.databaseUrl)
+
     companion object{
         //Instancia unica
         val instancia: Jdbi by lazy {
-            JdbiManager.jdbi
+            JdbiManager().jdbi
+        }
+    }
+    fun ejecutarSqlScript(scriptFilePath: String){
+        log.debug { "ejecutanto el script $scriptFilePath" }
+        val script = File(scriptFilePath).readText()
+        jdbi.useHandle<Exception>{ handle ->
+            handle.createScript(script).execute()
+
         }
     }
 
-    private val jdbi = Jdbi.create(Config.databaseUrl)
+    fun ejecutarScripCreacionTabla(tables: String) {
+        log.debug { "ejecutando el scrip de creacion de la tabla" }
+        val script = File(tables).readText()
+            jdbi.useHandle<Exception>{ handle ->
+                handle.createScript(script).execute()
+            }
+
+    }
+
+    init {
+        log.debug { "iniciando el jdbi" }
+
+        jdbi.installPlugin(KotlinPlugin())
+        jdbi.installPlugin(SqlObjectPlugin())
+
+        if (Config.databaseInitTables){
+            log.debug { "creando tablas" }
+            ejecutarScripCreacionTabla("tables.sql")
+        }
+    }
+
+
+
+
 }
